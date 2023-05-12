69deb535a15c61327eb720306859a4e96c157d1d
AritroB <77176367+AritroB@users.noreply.github.com>
2023-05-12 10:44:21 -0400

    created employee class

---
+++ BethanysPieShopHRM/BethanysPieShopHRM/Employe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace BethanysPieShopHRM
+{
+	internal class Employe
+	{
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numberOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+
+        public DateTime birthDay;
+
+    }
+}
+
